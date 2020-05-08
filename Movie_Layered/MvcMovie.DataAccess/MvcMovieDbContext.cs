0aff85feef674ae9f3fe9dc38a45934697807de0
sulav <sulav.aryal@outlook.com>
2020-05-07 22:01:24 -0700

    MvcMovie Seperation of Concern

---
+++ Movie_Layered/MvcMovie.DataAccess/MvcMovieDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcMovie.Domain;
+
+namespace MvcMovie.DataAccess
+{
+    public class MvcMovieDbContext:DbContext
+    {
+        public MvcMovieDbContext(DbContextOptions<MvcMovieDbContext> options) : base(options) { }
+        public DbSet<Movie> Movies { get; set; }
+
+    }
+}
